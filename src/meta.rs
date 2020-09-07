32a3a193418f1e6c0b0799ac3c0c63ba6525e1ad
liambloom <liamrbloom@gmail.com>
2020-09-07 16:33:28 -0400

    Added license notices to files

--- src/meta.rs
+++ src/meta.rs
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2020 Liam Bloom
+
+    This file is part of Choco.
+
+    Choco is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Choco is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Choco.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use toml::{map::Map, Value};
 
 pub fn package_data() -> Option<Map<String, Value>> {
